db9b61c0b98f78124c50704e96126130b5579f54
Jose Medellin <kabo.0699@gmail.com>
2021-06-23 08:45:08 -0500

    sign in design p1

--- lib/pages/components/login/signin/widgets/signin.widgets.dart
+++ lib/pages/components/login/signin/widgets/signin.widgets.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/shared/colors.dart';
 
+// ignore: non_constant_identifier_names
 Widget SignInForm( BuildContext context ){
 
   final size = MediaQuery.of(context).size;
@@ -15,82 +16,8 @@ Widget SignInForm( BuildContext context ){
             padding: EdgeInsets.symmetric( vertical: 50.0 ),
             child: Column(
               children: [
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    // labelText: 'Contraseña',
-                    filled: true,
-                    fillColor: Colors.white10,
-                    labelStyle: TextStyle(
-                      color: Colors.blueAccent,
-                      fontSize: 25
-                    ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never,
-                    hintText: 'Correo electronico',
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14
-                    ),
-                    alignLabelWithHint: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
-                        style: BorderStyle.none
-                      )
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(15.0),
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
-                        style: BorderStyle.solid
-                      )
-                    ),
-                    // suffix: Container(
-                    //   // margin: EdgeInsets.all(10),
-                    //   child: RaisedButton(
-                    //     shape: RoundedRectangleBorder(
-                    //       borderRadius: BorderRadius.circular(10.0),
-                    //       side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))
-                    //     ),
-                    //     onPressed: () {},
-                    //     color: Colors.white,
-                    //     textColor: Color.fromRGBO(0, 160, 227, 1),
-                    //     child: Text("Show",
-                    //       style: TextStyle(fontSize: 12)
-                    //     ),
-                    //   ),
-                    // ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  style: new TextStyle(
-                    fontFamily: "Poppins",
-                  ),
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: "Enter Email",
-                    fillColor: Colors.white,
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(25.0),
-                      borderSide: new BorderSide(
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(10.0),
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 3.0,
-                        style: BorderStyle.solid
-                      )
-                    )
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  style: new TextStyle(
-                    fontFamily: "Poppins",
-                  ),
-                ),
+                _emailInput(),
+                _passwordInput()
               ],
             ),
           ),
@@ -98,4 +25,129 @@ Widget SignInForm( BuildContext context ){
       ],
     ),
   );
+}
+
+Widget _passwordInput(){
+  return Container(
+    padding: EdgeInsets.all(8.00),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          child: Text(
+            'Contraseña',
+            style: TextStyle(
+              color: Colors.white70
+            ),
+          ),
+        ),
+        TextFormField(
+          decoration: new InputDecoration(
+            filled: true,
+            fillColor: Colors.white10,
+            hintText: 'Contraseña',
+            hintStyle: TextStyle(
+              color: Colors.white70,
+              fontSize: 14
+            ),
+            prefixIcon: Icon(
+              Icons.password,
+              color: Colors.white70,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+                style: BorderStyle.none
+              )
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(15.0),
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+                style: BorderStyle.solid
+              )
+            ),
+          ),
+          keyboardType: TextInputType.text,
+          style: new TextStyle(
+            fontFamily: "Poppins",
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          obscureText: true,
+          obscuringCharacter: '■',
+          cursorColor: Colors.white,
+          cursorRadius: Radius.circular(12),
+          cursorHeight: 24,
+          cursorWidth: 2.5,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _emailInput(){
+  return Container(
+    padding: EdgeInsets.all(8.00),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          child: Text(
+            'Correo electronico',
+            style: TextStyle(
+              color: Colors.white70
+            ),
+          ),
+        ),
+        new TextFormField(
+          decoration: new InputDecoration(
+            filled: true,
+            fillColor: Colors.white10,
+            hintText: 'Correo electronico',
+            hintStyle: TextStyle(
+              color: Colors.white70,
+              fontSize: 14
+            ),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Colors.white70,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+                style: BorderStyle.none
+              )
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: new BorderRadius.circular(15.0),
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+                style: BorderStyle.solid
+              )
+            ),
+          ),
+          keyboardType: TextInputType.emailAddress,
+          style: new TextStyle(
+            fontFamily: "Poppins",
+            color: Colors.white,
+            fontWeight: FontWeight.w600
+          ),
+          cursorColor: Colors.white,
+          cursorRadius: Radius.circular(12),
+          cursorHeight: 24,
+          cursorWidth: 2.5,
+        ),
+      ],
+    ),
+  );
+                
 }
